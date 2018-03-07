8e57dfc9598d43f05047221f8af319e18062152b
kumarbhupi <kumar.bhupi13@gmail.com>
2018-03-07 20:55:36 +0100

    Layouts Creados.

--- app/src/main/java/com/example/hh/salle_library/MainActivity.kt
+++ app/src/main/java/com/example/hh/salle_library/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.hh.salle_library
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO: https://developer.android.com/training/implementing-navigation/nav-drawer.html usando este
+        //TODO: https://developer.android.com/training/implementing-navigation/nav-drawer.html?hl=es-419
+        //TODO http://www.sgoliver.net/blog/interfaz-de-usuario-en-android-navigation-drawer-navigationview/
+
+        nav_view.setNavigationItemSelectedListener { item: MenuItem ->
+
+        }
+
+
+
+
     }
 }
