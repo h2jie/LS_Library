2a07aa5fb136dd126bbaa54a265a83cfa5b216a1
kumarbhupi <kumar.bhupi13@gmail.com>
2018-03-14 19:39:27 +0100

    Añadir arrayList arreglado

--- app/src/main/java/com/example/hh/salle_library/BookAdapter.kt
+++ app/src/main/java/com/example/hh/salle_library/BookAdapter.kt
@@ -8,8 +8,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
-import com.google.gson.Gson
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.book_item.view.*
 import java.io.File
 import java.io.IOException
@@ -18,7 +16,7 @@ import java.net.URL
 /**
  * Created by Bhupinder Kumar on 11/03/2018.
  */
-class BookAdapter(var myBookList : ArrayList<Book>, var mContext : Context) : RecyclerView.Adapter<BookAdapter.MyViewHolder>() {
+class BookAdapter(var myBookList : ArrayList<Book>, var mContext : Context) : RecyclerView.Adapter<BookAdapter.MyViewHolder>(){
     companion object {
         var imgUrl : URL? = null
         var itemAutor : String? = null
@@ -29,7 +27,7 @@ class BookAdapter(var myBookList : ArrayList<Book>, var mContext : Context) : Re
     }
 
 
-    class MyViewHolder (v : View) : RecyclerView.ViewHolder(v), View.OnClickListener{
+    class MyViewHolder (v : View) : RecyclerView.ViewHolder(v){
         var title = v.item_title
         var author = v.item_author
         var desc = v.item_desc
@@ -41,10 +39,7 @@ class BookAdapter(var myBookList : ArrayList<Book>, var mContext : Context) : Re
         var editor = sharedPref.edit()
         var context = v.context
 
-        init {
-            addbtn.setOnClickListener(this)
-        }
-        override fun onClick(p: View?) {
+        /*override fun onClick(p: View?) {
             var mBook : Book? = null
             mBook = Book(imgUrl!!, itemTitle!!, itemAutor!!, itemDate!!, itemDesc!!)
 
@@ -58,7 +53,7 @@ class BookAdapter(var myBookList : ArrayList<Book>, var mContext : Context) : Re
             }catch (e: IOException){
                 print(e.message)
             }
-        }
+        }*/
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyViewHolder{
@@ -78,13 +73,31 @@ class BookAdapter(var myBookList : ArrayList<Book>, var mContext : Context) : Re
         holder.date.text = book.bookDate
         holder.desc.text = book.bookDesc
         Glide.with(mContext).load(book.imageUrl).into(holder.bookImg)
+        holder.addbtn.setOnClickListener(View.OnClickListener { v->
+            var mUser = FirebaseAuth.getInstance().currentUser
+            var sharedPref = mContext.getSharedPreferences(mUser!!.uid,Context.MODE_PRIVATE)
+            var editor = sharedPref.edit()
+            var mBook : Book? = null
+            mBook = Book(book.imageUrl, book.bookTitle, book.bookAuthor, book.bookDate, book.bookDesc)
 
-        imgUrl = book.imageUrl
+
+            var mBooks = ArrayList<Book>()
+            try {
+                mBooks = ObjectSerializer.deserialize(sharedPref.getString("books",ObjectSerializer.serialize(ArrayList<Book>()))) as ArrayList<Book>
+                mBooks.add(mBook)
+                editor.putString("books",ObjectSerializer.serialize(mBooks))
+                editor.commit()
+                Toast.makeText(v!!.context,"Added ${mBook.bookTitle} to favorite",Toast.LENGTH_SHORT).show()
+            }catch (e: IOException){
+                print(e.message)
+            }
+        })
+
+        /*imgUrl = book.imageUrl
         itemAutor = book.bookAuthor
         itemDesc  = book.bookDesc
         itemDate  = book.bookDate
-        itemTitle = book.bookTitle
+        itemTitle = book.bookTitle*/
 
     }
-
 }
